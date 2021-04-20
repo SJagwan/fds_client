087d9a7e0f187860a3ff80a583b6af9a980e36e4
dhruvjoshi <dhruvjoshi0415@gmail.com>
2021-04-20 23:56:32 +0530

    Bill Ui Created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Nav from './Components/Nav'
 
 import { Form, Button } from 'react-bootstrap'
+import BillPage from './Pages/BillPage';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Nav />
+        <BillPage/>
 
       </div>
 
---
+++ src/Pages/BillPage.js
@@ -0,0 +1,13 @@
+import AddBill from "../Components/Bill/AddBill";
+
+const BillPage = () => {
+    return ( 
+        <div>
+            {
+                <AddBill/>
+            }
+        </div>
+     );
+}
+ 
+export default BillPage;

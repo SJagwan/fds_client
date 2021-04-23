99ef439a2034663510abc09951dd12a31ed3bbd2
tyagimayank027 <tyagimayank027@gmail.com>
2021-04-23 16:19:49 +0530

    working on redux

--- src/redux/restaurant/addRestaurant/addRestaurantAction.js
+++ src/redux/restaurant/addRestaurant/addRestaurantAction.js
@@ -1,15 +1,19 @@
 import { addRestaurantConstant } from "./addRestaurantConstant";
 
 export const addRestaurantStart=()=>({
-    type:addRestaurantConstant.Fetch_Add_Restaurant_Start
+    type:addRestaurantConstant.Fetch_Add_Restaurant_Start,
+    payload:undefined,
+    error:undefined
 })
 
 export const addRestaurantSuccess=()=>({
     typr:addRestaurantConstant.Fetch_Add_Restaurant_Success,
-    payload:data
+    payload:data,
+    error;undefined,
 })
 
 export const addRestaurantFailure=()=>({
     type:addRestaurantConstant.Fetch_Add_Restaurant_Failure,
-    payload:error
+    payload:undefined,
+    error:undefined
 })

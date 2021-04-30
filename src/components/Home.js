6e59088928ac4289b1583ce7d1c03ec814732805
SJagwan <shubhanshu1997jagwan@gmail.com>
2021-04-30 11:15:45 +0530

    update project name

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="container home">
         <div className="home-discription">
-            <h2> <span className="dis-span">Welcome to</span> Food Delivery Appication</h2>
+            <h2> <span className="dis-span">Welcome to</span> Food Delivery Application</h2>
             <p className="quote1">"Good Food never fails in bringing people together"</p>
         </div>
         <div className="fds-img"></div>
